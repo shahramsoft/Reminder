f64ea89070533ab0b5aeef4989e1f041bd4f9e81
Shahram Foroozan <shahram.soft@gmail.com>
2018-05-24 13:16:18 +0430

    AddCreate Device Api

---
+++ MobileApiGeneratorFramework/MobileApiGeneratorFramework/Controllers/Api/Reminder/CreateDeviceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GeneralLibrary.Token;
+using GeneralLibrary.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using ReminderLibrary.Logic;
+
+namespace MobileApiGeneratorFramework.Controllers.Api.Reminder
+{
+    [Produces("application/json")]
+    [Route("api/CreateDevice")]
+    public class CreateDeviceController : Controller
+    {
+        public ResultViewModel Post([FromBody] string deviceName, [FromBody]  int userId, [FromBody]  DateTime firstTimeChecked, [FromBody] int periodOfChecks, [FromBody] string token)
+        {
+            var accessTokenManager = new TokenManager();
+            // اگر توکن در خواست با مقدار موجود در دیتابیس مطابقت نداشت یا تاریخ مصرف آن به پایان رسیده بود از دسترسی به ای پی آی جلوگیری می شود
+            if (accessTokenManager.GetToken(token) == false)
+            {
+                return new ResultViewModel
+                {
+                    Validate = false,
+                    ValidateMessage = "Invalid Token",
+                    InvalidToken = true
+                };
+            }
+            var userMng = new DeviceManager();
+            var result = userMng.CreateDevice(deviceName, userId, firstTimeChecked, periodOfChecks);
+            return result;
+        }
+    }
+}
--- MobileApiGeneratorFramework/ReminderLibrary/Logic/DeviceManager.cs
+++ MobileApiGeneratorFramework/ReminderLibrary/Logic/DeviceManager.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using GeneralLibrary.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReminderLibrary.Logic
 {
-   public class DeviceManager
+   public class DeviceManager:GeneralLibrary.ConnectionManager
     {
+        public DeviceManager():base()
+        {
+
+        }
+        public ResultViewModel CreateDevice (string deviceName, int userId,DateTime firstTimeChecked,int periodOfChecks)
+        {
+            try
+            {
+                var commandnitgen = @"insert into Device (UserId,DeviceName,FirstTimeCheck,PeriodOfChecksPerMonths) values ("+ userId + ",'"+ deviceName + "','"+ firstTimeChecked + "',"+ periodOfChecks + ")";
+                var conn = new SqlConnection(ConnectionString);
+
+                using (var cmd = new SqlCommand(commandnitgen, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    conn.Open();
+                    var reader = cmd.ExecuteNonQuery();
+
+                    conn.Close();
+                    conn.Dispose();
+                    //reader.Close();
+                }
+                return new ResultViewModel
+                {
+                    Validate = true,
+                    //Message = JsonConvert.SerializeObject(userObj)
+                    ValidateMessage = "ثبت با موفقیت به تمام رسید"
+
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResultViewModel
+                {
+                    Validate = false,
+                    Message = "",
+                    ValidateMessage = "عملیات با خطا مواجه شد",
+                    ExceptionMessage = ex.Message
+                };
+            }
+        }
     }
 }
